b4bac4503e303155411c12f17974c5ed3e25780e
wilmot <wilmot@fias.uni-frankfurt.de>
2018-10-24 19:38:18 +0200

    new prototypes

--- src/dataio.py
+++ src/dataio.py
@@ -3,3 +3,19 @@
 #
 #  It can in particular read and write matlab or python matrices.
 import numpy as np
+import logging as log
+
+
+logger = log.getLogger("classifier")
+
+
+def get_data(args):
+    logger.info("Reading input data")
+    # input_data["train"]
+    # input_data["test"]
+    logger.info("Input data read")
+
+
+def save(args, output_data, graph):
+    logger.info("Saving output data")
+    logger.info("Output data daved to {}".format("TODO"))
